775195173d9c585d8d3db5a58bcc6fd5c60c0d63
tianhua <tianhua.tang@verizonconnect.com>
2019-05-03 15:29:29 +0100

    update

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -8,13 +8,13 @@ import { ProductService } from 'app/product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  public albumInfo: any;
+  albumInfo;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
-    console.log(this.albumInfo)
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
   }
 
 }
